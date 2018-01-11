a2318dd09a2db4d5c7bd2b93dfcb670c15f3ba8b
Jordi <jbartual@gmail.com>
2018-01-11 08:17:54 +0100

    Second commit

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -6,7 +6,9 @@ contract Splitter {
     address public alice;
     address public bob;
     address public carol;
-  
+
+    uint private contractBalance;
+
     function Splitter(address _alice, address _bob, address _carol) public {
         owner = msg.sender;
         alice = _alice;
@@ -14,8 +16,18 @@ contract Splitter {
         carol = _carol;
     }
 
+    function () public payable {
+        //This function is needed to allow the contract to store value
+        contractBalance += msg.value;
+    }
+
+    function kill() public payable returns(bool) {
+        selfdestruct(owner);
+        return true;
+    }
+
     function getContractBalance() returns (uint) {
-        return owner.balance;
+        return this.balance;
     }
 
     function getAliceBalance() returns (uint) {
@@ -33,19 +45,16 @@ contract Splitter {
     function sendFunds () public payable returns (bool) {
         require(msg.value > 0); //avoid 0 value transfers
         require(msg.sender.balance >= msg.value); //avoid overspending
-         
+
          if (msg.sender == alice) { //if sender is Alice then the funds are split between Bob and Carol
             bob.transfer(msg.value/2);
             carol.transfer(msg.value/2);
-         } else { //else, funds go to the contract
-            owner.transfer(msg.value);
+         } else {
+            //else, funds go to the contract
+            //update the private variable that keeps track of the available balance
+            contractBalance += msg.value;
          }
 
-        return true;
-    }
-
-    function kill() public payable returns(bool) {
-        selfdestruct(owner);
         return true;
     }
 }
--- test/Splitter.js
+++ test/Splitter.js
@@ -10,12 +10,14 @@ contract('Splitter', function(accounts) {
   var alice_balance_start;
   var bob_balance_start;
   var carol_balance_start;
+  var contract_balance_start;
 
   var alice_balance_ending;
   var bob_balance_ending;
   var carol_balance_ending;
+  var contract_balance_ending;
 
-  var transferAmount = web3.toWei(2, "ether");
+  var transferAmount = web3.toWei(1, "ether");
 
   before (function(){
     return Splitter.new(alice_account, bob_account, carol_account, {from:accounts[0]}).then(function(i) {
@@ -24,7 +26,6 @@ contract('Splitter', function(accounts) {
   });
 
   it("Alice tranfer should send half to Bob and Carol", function() {
-
     return instance.getAliceBalance.call().then (function(balance) {
       alice_balance_start = balance.toNumber();
       return instance.getBobBalance.call();
@@ -33,10 +34,11 @@ contract('Splitter', function(accounts) {
       return instance.getCarolBalance.call();
     }).then (function(balance){
       carol_balance_start = balance.toNumber();
-    })
-      
-    var tx = instance.sendFunds({from: alice_account, value:transferAmount});
-    
+      return instance.sendFunds({from: alice_account, value:transferAmount});
+    }).then(function(){});
+  });
+
+  it("  Check Alice transfer was ok", function() {
     return instance.getAliceBalance.call().then (function(balance){
       alice_balance_ending = balance.toNumber();
       return instance.getBobBalance.call();
@@ -45,35 +47,28 @@ contract('Splitter', function(accounts) {
       return instance.getCarolBalance.call();
     }).then (function(balance){
       carol_balance_ending = balance.toNumber();
-    })    
-
-    assert.equal(bob_balance_start, bob_balance_ending - (transferAmount/2), "Bob's account wasn't credited correctly");
-    assert.equal(carol_balance_start, carol_balance_ending - (transferAmount/2), "Carol's account wasn't credited correctly");
-
-  })
-
-  it("Bob transfer is too expensive", function(){
-    return instance.getBobBalance.call().then(function(balance){
-      bob_balance_start = balance.toNumber();
+      assert.equal(bob_balance_start.toFixed(5), bob_balance_ending.toFixed(5) - (transferAmount/2).toFixed(5), "Bob's account wasn't credited correctly");
+      assert.equal(carol_balance_start.toFixed(5), carol_balance_ending.toFixed(5) - (transferAmount/2).toFixed(5), "Carol's account wasn't credited correctly");
     })
-
-    var tx = instance.sendFunds({from:bob_account, value:transferAmount*bob_balance_start}); 
-
-    return instance.getBobBalance.call().then(function(balance){
-      assert.equal(balance.toNumber(), bob_balance_start, "Bob's balance has changed. The transaction succeed when it was expected to fail");
-    })
-  })
+  });
 
   it("Carol transfers to Contract successfully", function(){
     return instance.getCarolBalance.call().then(function(balance){
-      carol_balance_start = balance;
-    })
-
-    var tx = instance.sendFunds({from:carol_account, value:carol_balance_start/3});
-
-    return instance.getCarolBalance.call().then(function(balance){
-      assert.isBelow(balance.toNumber(), carol_balance_start, "Carol's new balance after transfer is not lower than before");
-    })
-  })
+      carol_balance_start = balance.toNumber();
+      return instance.getContractBalance.call();
+    }).then(function(balance) {
+      contract_balance_start = balance.toNumber();
+      return instance.sendFunds({from:carol_account, value:carol_balance_start/3});
+    }).then(function(){
+      return instance.getCarolBalance.call();
+    }).then(function(balance){
+      carol_balance_ending = balance.toNumber();
+      return instance.getContractBalance.call();
+    }).then(function(balance) {
+      contract_balance_ending = balance.toNumber();
+      assert.isBelow(carol_balance_ending.toFixed(5), carol_balance_start.toFixed(5), "Carol's new balance after transfer is not lower than before");
+      assert.isAbove(contract_balance_ending.toFixed(5), contract_balance_start.toFixed(5), "Contract's balance didn't increase with Carol's transfer");
+      })
+  });
 
 });
