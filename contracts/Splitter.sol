414df94a68d51dff84b0eae516132a3b8a552474
Jordi <jbartual@gmail.com>
2018-01-14 09:59:55 +0100

    Added softKill functionality and other improvements

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -28,42 +28,104 @@ contract Splitter {
     address public bob;
     address public carol;
 
+    bool private isKilled;
+
     function Splitter(address _alice, address _bob, address _carol) public {
+        require(_alice != address(0));
+        require(_bob != address(0));
+        require(_carol != address(0));
+
         owner = msg.sender;
         alice = _alice;
         bob = _bob;
         carol = _carol;
+
+        isKilled = false;
     }
 
-    function kill() public {
+    modifier onlyOwner() {
         require(msg.sender == owner);
+        _;
+    }
+
+    modifier onlyParticipants() {
+        require (msg.sender == alice || msg.sender == bob || msg.sender == carol || msg.sender == owner);
+        _;
+    }
+
+    function kill() onlyOwner public {
         selfdestruct(owner);
     }
 
-    function getContractBalance() returns (uint) {
+    function softKill() onlyOwner public returns(bool) {
+        isKilled = true;
+        return true;
+    }
+
+    function softResurrect() onlyOwner public returns(bool) {
+        isKilled = false;
+        return true;
+    }
+
+    function getIsKilled () public returns(bool) {
+        return isKilled;
+    }
+
+    function isKilledRefund (address _address, uint _amount) private {
+        require (isKilled);
+        _address.transfer(_amount);
+    }
+
+    function refund (address _address, uint _amount) onlyOwner public returns (bool) {
+        //Is possible that someone sends funds to the contract by mistake. This function allows the Owner to refund a given account
+        require (_address != address(0));
+        _address.transfer(_amount);
+        return true;
+    }
+
+    function getContractBalance() onlyOwner public returns (uint) {
         return this.balance;
     }
 
-    function getAliceBalance() returns (uint) {
-        return alice.balance;
+    function getBalance(address _address) onlyParticipants public returns (uint) {
+        return _address.balance;
     }
 
-    function getBobBalance() returns (uint) {
-        return bob.balance;
-    }
+    function sendFunds () onlyParticipants public payable returns (bool) {
+        //Only participants can use this function to send funds to the contract
+        //If Alice is using this function the funds will be split half to Bob and the other half to Carol
 
-    function getCarolBalance() returns (uint) {
-        return carol.balance;
-    }
-
-    function sendFunds () public payable returns (bool) {
         require(msg.value > 0); //avoid 0 value transfers
 
-         if (msg.sender == alice) { //if sender is Alice then the funds are split between Bob and Carol, else funds go to the contract
-            bob.transfer(msg.value/2);
-            carol.transfer(msg.value/2);
-         }
+        if (isKilled) {
+            isKilledRefund (msg.sender, msg.value);
+            return false;
+        } else {
+            if (msg.sender == alice) { //if sender is Alice then the funds are split between Bob and Carol, else funds go to the contract
+                require (msg.value % 2 == 0); //Divisible
+                bob.transfer(msg.value/2);
+                carol.transfer(msg.value/2);
+            }
+            return true;
+        }
+    }
 
-        return true;
+    function splitFunds (address _address1, address _address2) public payable returns (bool) {
+        //Anybody can use this function to split funds between two given addresses
+
+        require(msg.value > 0); //avoid 0 value transfers
+
+        if (isKilled) {
+            isKilledRefund (msg.sender, msg.value);
+            return false;
+        } else {
+            require(msg.value % 2 == 0); //Divisible
+            require(_address1 != address(0));
+            require(_address2 != address(0));
+
+            _address1.transfer (msg.value / 2);
+            _address2.transfer (msg.value / 2);
+            return true;
+        }
     }
 }
--- test/Splitter.js
+++ test/Splitter.js
@@ -3,6 +3,7 @@ var Splitter = artifacts.require("./Splitter.sol");
 contract('Splitter', function(accounts) {
   var instance;
 
+  var owner_account = accounts[0];
   var alice_account = accounts[1];
   var bob_account = accounts[2];
   var carol_account = accounts[3];
@@ -27,14 +28,14 @@ contract('Splitter', function(accounts) {
 
   it("Alice tranfer should send half to Bob and Carol", function() {
     console.log("it: Alice tranfer should send half to Bob and Carol...");
-    return instance.getAliceBalance.call().then (function(balance) {
+    return instance.getBalance.call(alice_account).then (function(balance) {
       alice_balance_start = web3.fromWei(balance, "ether");
       console.log("Alice start = " + alice_balance_start.toString());
-      return instance.getBobBalance.call();
+      return instance.getBalance.call(bob_account);
     }).then (function(balance) {
       bob_balance_start = web3.fromWei(balance, "ether");
       console.log("Bob start = " + bob_balance_start.toString());
-      return instance.getCarolBalance.call();
+      return instance.getBalance.call(carol_account);
     }).then (function(balance){
       carol_balance_start = web3.fromWei(balance, "ether");
       console.log("Carol start = " + carol_balance_start.toString());
@@ -44,15 +45,15 @@ contract('Splitter', function(accounts) {
       console.log("Contract start = " + contract_balance_start.toString());
       return instance.sendFunds({from: alice_account, value:web3.toWei(transferAmount, "ether")});
     }).then(function(){
-      return instance.getAliceBalance.call()
+      return instance.getBalance.call(alice_account)
     }).then (function(balance){
       alice_balance_ending = web3.fromWei(balance, "ether");
       console.log("Alice ending = " + alice_balance_ending.toString());
-      return instance.getBobBalance.call();
+      return instance.getBalance.call(bob_account);
     }).then (function(balance){
       bob_balance_ending = web3.fromWei(balance, "ether");
       console.log("Bob ending = " + bob_balance_ending.toString());
-      return instance.getCarolBalance.call();
+      return instance.getBalance.call(carol_account);
     }).then (function(balance){
       carol_balance_ending = web3.fromWei(balance, "ether");
       console.log("Carol ending = " + carol_balance_ending.toString());
@@ -61,16 +62,16 @@ contract('Splitter', function(accounts) {
       contract_balance_ending = web3.fromWei(balance, "ether");
       console.log("Contract ending = " + contract_balance_ending.toString());
 
-      assert.isBelow(alice_balance_ending.toNumber().toFixed(5), alice_balance_start.toNumber().toFixed(5), "Alice's account wasn't debited correctly");
-      assert.isBelow(bob_balance_start.toNumber().toFixed(5), bob_balance_ending.toNumber().toFixed(5), "Bob's account wasn't credited correctly");
-      assert.isBelow(carol_balance_start.toNumber().toFixed(5), carol_balance_ending.toNumber().toFixed(5), "Carol's account wasn't credited correctly");
-      assert.equal(contract_balance_start.toNumber().toFixed(5), contract_balance_ending.toNumber().toFixed(5), "Carol's account wasn't credited correctly");
+      assert.isBelow(+alice_balance_ending.toNumber().toFixed(5), +alice_balance_start.toNumber().toFixed(5), "Alice's account wasn't debited correctly");
+      assert.isBelow(+bob_balance_start.toNumber().toFixed(5), +bob_balance_ending.toNumber().toFixed(5), "Bob's account wasn't credited correctly");
+      assert.isBelow(+carol_balance_start.toNumber().toFixed(5), +carol_balance_ending.toNumber().toFixed(5), "Carol's account wasn't credited correctly");
+      assert.equal(+contract_balance_start.toNumber().toFixed(5), +contract_balance_ending.toNumber().toFixed(5), "Carol's account wasn't credited correctly");
     })
   });
 
   it("Carol transfers to Contract successfully", function(){
     console.log("it: Carol transfers to Contract successfully...");
-    return instance.getCarolBalance.call().then(function(balance){
+    return instance.getBalance.call(carol_account).then(function(balance){
       carol_balance_start = web3.fromWei(balance, "ether");
       console.log("Carol start = " + carol_balance_start.toString());
       return instance.getContractBalance.call();
@@ -79,7 +80,7 @@ contract('Splitter', function(accounts) {
       console.log("Contract start = " + contract_balance_start.toString());
       return instance.sendFunds({from:carol_account, value:web3.toWei(carol_balance_start/3, "ether")});
     }).then(function(){
-      return instance.getCarolBalance.call();
+      return instance.getBalance.call(carol_account);
     }).then(function(balance){
       carol_balance_ending = web3.fromWei(balance, "ether");
       console.log("Carol ending = " + carol_balance_ending.toString());
@@ -88,9 +89,36 @@ contract('Splitter', function(accounts) {
       contract_balance_ending = web3.fromWei(balance, "ether");
       console.log("Contract ending = " + contract_balance_ending.toString());
 
-      assert.isBelow(carol_balance_ending.toNumber().toFixed(5), carol_balance_start.toNumber().toFixed(5), "Carol's new balance after transfer is not lower than before");
-      assert.isAbove(contract_balance_ending.toNumber().toFixed(5), contract_balance_start.toNumber().toFixed(5), "Contract's balance didn't increase with Carol's transfer");
-      })
+      assert.isBelow(+carol_balance_ending.toNumber().toFixed(5), +carol_balance_start.toNumber().toFixed(5), "Carol's new balance after transfer is not lower than before");
+      assert.isAbove(+contract_balance_ending.toNumber().toFixed(5), +contract_balance_start.toNumber().toFixed(5), "Contract's balance didn't increase with Carol's transfer");
+    });
+  });
+
+  it("Soft kill the contract. Try to send funds. Funds shall bounce back", function() {
+    console.log("it:Soft kill the contract. Try to send funds. Funds shall bounce back");
+    return instance.getContractBalance.call().then(function(balance) {
+      contract_balance_start = web3.fromWei(balance, "ether");
+      console.log("Contract start = " + contract_balance_start.toString());
+      console.log("Soft kill the contract...");
+      return instance.softKill({from:owner_account});
+    }).then(function(){
+      return instance.getBalance.call(carol_account);
+    }).then(function(balance){
+      carol_balance_start = web3.fromWei(balance, "ether");
+      console.log("Carol start = " + carol_balance_ending.toString());
+      console.log("Carol sends funds of 0.1 ether");
+      return instance.sendFunds({from:carol_account, value:web3.toWei(0.1, "ether")});
+    }).then(function(){
+      return instance.getBalance.call(carol_account);
+    }).then(function(balance){
+      carol_balance_ending = web3.fromWei(balance, "ether");
+      console.log("Carol ending = " + carol_balance_ending.toString());
+      return instance.getContractBalance.call();
+    }).then(function(balance) {
+      contract_balance_ending = web3.fromWei(balance, "ether");
+      console.log("Contract ending = " + contract_balance_ending.toString());
+      assert.equal(+contract_balance_start, +contract_balance_ending, "Contract balance shouldn't have changed");
+    });
   });
 
 });
